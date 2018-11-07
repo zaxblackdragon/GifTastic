cbd9fd10b584749c6dc5267e939e749272241f50
Zac Seewald <zs@Zacs-MacBook-Pro.local>
2018-11-06 22:28:09 -0600

    buttons are generating on load and and set the url variable.  added css styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+    // -> steps
+    //      1. load pre-existing buttons
+    //          button variable - DONE
+    //              add attr to buttom
+
+    //              var with attr
+    //              for loop to add type to buttons
+
+    var preExButtons = ["car ", "spaceship", "cat", "monkey"];
+
+    function addType() {
+        $("#buttons").empty();
+        for (var i =0; i < preExButtons.length; i++) {
+            var button = $("<button>");
+            button.addClass("btn btn-primary");
+            button.attr("data-type", preExButtons[i]);
+            button.text(preExButtons[i]);
+            $("#buttons").append(button);
+        }
+
+    }
+    addType();
+
+    
+    //      2. on button click - pop giphy field from giphy api
+    //              event listener -> waits for button clicked
+    //                      onclick ->  read buttun type
+    //                          feed type to the api  and request the respone
+    //                          pop gif field with resposne
+    $(document).on("click", "button", function() {
+        
+        var giffyReq = $(this).attr("data-type");
+        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + giffyReq;
+ 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+
+            var imageUrl = response.data.url;
+              console.log(response.data.url);
+            //$("#gifContainer").append()
+          })
+    });
+
+    //      3. user creates button that works the same as pre-existing buttons
+    //              event listener tied to the user input ie submit button
+    //                         push user input to the button variable from step  1
+    //                          call func from step 1
+    
+}); // -> doc ready close
