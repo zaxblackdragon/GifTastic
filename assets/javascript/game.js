17a01d1d42e5e4633c430048fd0dd37f7c4358b6
Zac Seewald <zs@Zacs-MacBook-Pro.local>
2018-11-07 20:09:11 -0600

    submit button is buggy, reloads the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,44 @@
 $(document).ready(function() {
-    // -> steps
-    //      1. load pre-existing buttons
-    //          button variable - DONE
-    //              add attr to buttom
-
-    //              var with attr
-    //              for loop to add type to buttons
-
+// -> onload button names
     var preExButtons = ["SNL ", "Spaceship", "Horse", "Iguana", "Uggly Cry", "Batman", "Bob Ross"];
-
+// -> func for creating buttons from the array
     function addType() {
         $("#buttons").empty();
         for (var i =0; i < preExButtons.length; i++) {
+            // stores the btn tag
             var button = $("<button>");
+            // adds a class to the btn
             button.addClass("btn btn-primary");
+            // adds data type attr to btn = var[i] so it can be uniquely identified and used later
             button.attr("data-type", preExButtons[i]);
+            // put the text on the btn = var[i]
             button.text(preExButtons[i]);
+            // adds the btn to the page
             $("#buttons").append(button);
         }
 
     }
+// -> calls the func to add btn at page onload
     addType();
 
-    
-    //      2. on button click - pop giphy field from giphy api
-    //              event listener -> waits for button clicked
-    //                      onclick ->  read buttun type
-    //                          feed type to the api  and request the respone
-    //                          pop gif field with resposne
-    $(document).on("click", "button", function() {
 
+
+// -> onclick event listens for any button clicked
+    $("#buttons").on("click", "button", function() {
+// ->   adds an attr and stores it in the var
         var giffyReq = $(this).attr("data-type");
+//  ->      stores the api req
         var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + giffyReq;
- 
+ // -> call
         $.ajax({
             url: queryURL,
             method: "GET"
           }).then(function(response) {
-            // set the image location to a var
+            // set the response to a var
             var imageUrl = response.data.image_original_url;
             // adds an image tag to a var
             var gifImage = $("<img>");
-             // console.log(response.data.url);
+             // adds attr to the img tag
              gifImage.attr("src", imageUrl)
              gifImage.attr("id", "gif-style")
              
@@ -50,10 +47,18 @@ $(document).ready(function() {
             $("#gifContainer").prepend(gifImage);
           })
     });
-
+    // $(document).on("click", "button", function() {
+    //     console.log("test");
+    // }
     //      3. user creates button that works the same as pre-existing buttons
     //              event listener tied to the user input ie submit button
     //                         push user input to the button variable from step  1
     //                          call func from step 1
+ 
+
+    // testing onclick for submit button -> it resets the page when clicked
+    $("form").on("click", "button", function() {
+        console.log("test");
+    })
     
 }); // -> doc ready close
